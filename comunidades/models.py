e66b493be1d510f7132c3d6b554dd2724bdfc27f
Telnesito <carlosj_1703@hotmail.com>
2023-11-24 21:43:41 -0400

    Modelo comunidades

--- comunidades/models.py
+++ comunidades/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from usuarios.models import Usuarios
 # Create your models here.
+
+class Comunidades(models.Model):
+  idComunidad = models.AutoField(primary_key=True)
+  cedulaCreador =  models.OneToOneField(Usuarios, to_field='cedula', on_delete=models.CASCADE,)
+  nombreComunidad = models.CharField(max_length=144)
+  descripcionComunidad = models.TextField()
+  fechaCreada = models.DateTimeField(auto_created=True)
+  
