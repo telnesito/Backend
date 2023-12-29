bd8bfcb04fbd11a017c3b11c28208d9135aa57f3
Telnesito <carlosj_1703@hotmail.com>
2023-12-29 14:44:52 -0400

    algo que me ense;o un indu a las  3 de la madrugada

--- publicacion/views.py
+++ publicacion/views.py
@@ -27,6 +27,21 @@ class PublicacionViewSet(viewsets.ModelViewSet):
 class VideosPublicacionViewSet(viewsets.ModelViewSet):
   queryset = capitulosPublicacion.objects.all()
   serializer_class = VideosPublicacionSerializer
+  parser_classes  = (MultiPartParser, FormParser)
+  
+  def create(self, request, *args, **kwargs):
+   idPublicacion_id = request.data['idPublicacion']
+   tituloCapitulo = request.data['tituloCapitulo']
+   descripcionCapitulo =  request.data['descripcionCapitulo']
+   miniatura = request.data['miniatura']
+   video_url = request.data['video_url']
+   
+   idPublicacion = Publicacion.objects.get(idPublicacion=idPublicacion_id)
+   
+   
+   capitulosPublicacion.objects.create(idPublicacion = idPublicacion, tituloCapitulo = tituloCapitulo, descripcionCapitulo = descripcionCapitulo, video_url = video_url, miniatura = miniatura)
+   return Response('Video agregado..')
+   
 
 class ComentariosViewSet(viewsets.ModelViewSet):
   queryset = Comentarios.objects.all()
